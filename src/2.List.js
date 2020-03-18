5b6911fed3721ebdc8189817e455d9de49a7c62d
szy0syz <szy0syz@gmail.com>
2020-03-19 07:50:34 +0800

    Update 2-List

--- src/2.List.js
+++ src/2.List.js
@@ -1,10 +1,5 @@
-# hi-immutableJS
+// List
 
-> learning immutable.js
-
-## List
-
-```js
 //! 创建List，注意：List是工厂方法，不能使用 new 初始化
 const { List } = require('immutable');
 
@@ -19,4 +14,10 @@ console.log('isList', List.isList([1, 2, 3])); // -> false
 
 // List.of() 创建新List
 const l = List.of(1, 2, 3, 4);
-```
+
+// size 属性取得 List 的长度
+console.log('size:', l.size);
+
+// set 方法用来设定指定下表的值 set(ind, val)
+// l.set(0, 110);
+// l.set(6, 666); //* 将 List 扩展到size为6，设置索引6为666，中间没有的 undefined
